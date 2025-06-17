e472654a0147af2b370a0d080ee52ba1ec12425d
Zain Saqib <xain.saqib@gmail.com>
2025-06-17 12:33:22 +0500

    added router links to form different pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';      
+import { Blog } from './pages/blog/blog';
+import { AddPost } from './pages/add-post/add-post';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'blog/:id', component: Blog },
+  { path: 'add-post', component: AddPost },
+  { path: '**', redirectTo: '' } 
+];
--- src/app/pages/add-post/add-post.ts
+++ src/app/pages/add-post/add-post.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  
   selector: 'app-add-post',
   imports: [],
   templateUrl: './add-post.html',
