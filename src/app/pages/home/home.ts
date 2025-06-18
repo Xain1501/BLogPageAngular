727d758749d3cd89e24e27b7aa1b623a4df735c7
Zain Saqib <xain.saqib@gmail.com>
2025-06-18 09:53:50 +0500

    Added home and add-post functionality can add post and view it in home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,18 +1,27 @@
 import { Component, computed} from '@angular/core';
 import { PostService } from '../../services/post';
+import { CommonModule } from '@angular/common'
+import { Post } from '../../models/post.model';
+import { Signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
 
    
-  posts = computed(() => this.postService.getPosts()); 
+  // posts = computed(() => this.postService.getPosts()); 
   
-  constructor(private postService:PostService) {
+  // constructor(private postService:PostService) {
 
+  // }
+
+    posts: Signal<Post[]>;
+
+  constructor(private postService: PostService) {
+    this.posts = this.postService.getPosts(); // ✅ This is a signal
   }
 }
